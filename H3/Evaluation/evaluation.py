2710eb06dd683ca70f941b25096b4e9ced6ce1a5
Alexandru Oloieri <alexoloieri2014@gmail.com>
2021-03-27 22:29:00 +0200

    H3: Body parse error fix for Evaluator

--- H3/Evaluation/evaluation.py
+++ H3/Evaluation/evaluation.py
@@ -4,10 +4,10 @@ EVALUATION_KIND = 'Evaluation'
 
 def evaluate(evaluation_id):
     client = datastore.Client()
-    evaluation_obj_key = client.key(EVALUATION_KIND, evaluation_id)
+    evaluation_obj_key = client.key(EVALUATION_KIND, int(evaluation_id))
     evaluation_obj = client.get(evaluation_obj_key)
     if evaluation_obj is None:
         return ("Invalid evaluation_id in body! The evaluation doesn't exist!", 400)
-    
+
     print(evaluation_obj)
     return ("The evaluation was completed successfully!", 200)
--- H3/Evaluation/validator.py
+++ H3/Evaluation/validator.py
@@ -2,11 +2,21 @@ from cerberus import Validator
 
 EVALUATION_REQUEST_SCHEMA = {
     "evaluation_id": {
-        'type': 'string',
-        'required': True
+        'type': ('integer', 'string'),
+        'required': True,
+        'empty': True
     }
 }
 
+def is_int(evaluation_id):
+    evaluation_id_str = str(evaluation_id)
+    if len(evaluation_id_str) == 0 or evaluation_id_str == '-':
+        return False
+    start_from = 1 if evaluation_id_str[0] in ('-', '+') else 0
+    return evaluation_id_str[start_from:].isdigit()    
 
 def validate_body(body):
-    return Validator(EVALUATION_REQUEST_SCHEMA).validate(body)
+    if not Validator(EVALUATION_REQUEST_SCHEMA).validate(body):
+        return False
+    return is_int(body['evaluation_id'])
+    
