805f38ae3481669805ecc7ab1ed74a51855d96fc
Alexandru Oloieri <alexoloieri2014@gmail.com>
2021-04-20 17:50:12 +0300

    H4: Fixed web app navigation bug

--- H3/WebApp/src/app/services/auth.service.ts
+++ H3/WebApp/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 import { map, catchError } from 'rxjs/operators';
 
 import { GenericService } from './generic.service';
@@ -16,8 +17,9 @@ export class AuthService {
 
   constructor(
     private baseService: GenericService,
-    private jwtHelper: JwtHelperService
-  ) { }
+    private jwtHelper: JwtHelperService,
+    private router: Router
+  ) {}
 
   public login(credentials: UserCredentials) {
     return this.baseService.post(this.URL, 'login', credentials).pipe(
@@ -35,23 +37,24 @@ export class AuthService {
   }
 
   public logout() {
-    this.baseService.delete(this.URL, 'logout', { token: localStorage.getItem("refresh_token") });
+    this.baseService.delete(this.URL, 'logout', {
+      token: localStorage.getItem('refresh_token'),
+    });
     localStorage.removeItem('access_token');
     localStorage.removeItem('refresh_token');
-    window.location.reload();
+    this.router.navigate(['auth']);
   }
 
   public async isLoggedIn(): Promise<boolean> {
     const token: string = this.jwtHelper.tokenGetter();
 
-    if (!token && !localStorage.getItem("refresh_token")) {
+    if (!token && !localStorage.getItem('refresh_token')) {
       return false;
     }
 
     if (this.jwtHelper.isTokenExpired()) {
       localStorage.removeItem('access_token');
       const result = await this.getNewAccessToken().toPromise();
-      console.log(result)
       return false;
     }
 
@@ -59,18 +62,19 @@ export class AuthService {
   }
 
   public getNewAccessToken() {
-    return this.baseService.post(this.URL, 'token', { token: localStorage.getItem("refresh_token") }).pipe(
-      map((response: any): any => {
-        console.log(response);
-        if (response && response.accessToken) {
-          localStorage.setItem('access_token', response.accessToken);
-          return { status: true };
-        }
-        return { status: false };
-      }),
-      catchError((err: any): any => {
-        return { status: false };
-      })
-    );
+    return this.baseService
+      .post(this.URL, 'token', { token: localStorage.getItem('refresh_token') })
+      .pipe(
+        map((response: any): any => {
+          if (response && response.accessToken) {
+            localStorage.setItem('access_token', response.accessToken);
+            return { status: true };
+          }
+          return { status: false };
+        }),
+        catchError((err: any): any => {
+          return { status: false };
+        })
+      );
   }
 }
