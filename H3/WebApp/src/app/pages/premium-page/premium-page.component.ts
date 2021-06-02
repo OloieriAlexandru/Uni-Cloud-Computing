f822ffece7b7191d933dc86e981859d8409f1635
Alexandru Oloieri <alexoloieri2014@gmail.com>
2021-06-02 21:09:25 +0300

    [AzureCloud] Web App - Updated the urls of the payments microservice, small UI improveements

--- H3/WebApp/src/app/pages/premium-page/premium-page.component.ts
+++ H3/WebApp/src/app/pages/premium-page/premium-page.component.ts
@@ -1,29 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { Router } from '@angular/router';
-import { SubscriptionPrices } from 'src/app/models/SubscriptionPrices';
-import { PaymentPageComponent } from '../../components/payment-popup/payment.component';
 
+import { SubscriptionPrices } from 'src/app/models/SubscriptionPrices';
+
+import { PaymentPageComponent } from '../../components/payment-popup/payment.component';
 
 @Component({
   selector: 'app-premium-page',
   templateUrl: './premium-page.component.html',
   styleUrls: ['./premium-page.component.scss'],
 })
-export class PremiumPageComponent implements OnInit {
-  public monthly =  SubscriptionPrices.MONTHLY;
+export class PremiumPageComponent {
+  public monthly = SubscriptionPrices.MONTHLY;
   public yearly = SubscriptionPrices.YEARLY;
-  constructor(private readonly matDialog: MatDialog) { }
-  ngOnInit(): void { }
-  checkout(amount:SubscriptionPrices) {
+
+  constructor(private readonly matDialog: MatDialog) {}
+
+  checkout(amount: SubscriptionPrices) {
     const dialogRef = this.matDialog.open(PaymentPageComponent, {
       data: { totalAmount: amount },
     });
-    dialogRef.afterClosed()
-      .subscribe((result: any) => {
-        if (result) {
-          console.log(result.token.id);
-        }
-      });
+    dialogRef.afterClosed().subscribe((result: any) => {
+      if (result) {
+        console.log(result.token.id);
+      }
+    });
   }
 }
--- H3/WebApp/src/app/services/payment.service.ts
+++ H3/WebApp/src/app/services/payment.service.ts
@@ -1,22 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { environment } from 'src/environments/environment';
-
 import { GenericService } from './generic.service';
+
 import { PaymentData } from '../models/PaymentData';
+
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaymentService {
-  private URL = environment.paymentURL;
+  private URL = environment.paymentsURL;
 
   constructor(private baseService: GenericService) {}
 
   public processPayment(paymentData: PaymentData): Observable<any> {
-    console.log(paymentData);
-    return this.baseService.postAlt<PaymentData>(this.URL, '/charge', paymentData);
+    return this.baseService.postAlt<PaymentData>(
+      this.URL,
+      '/charge',
+      paymentData
+    );
   }
-
 }
