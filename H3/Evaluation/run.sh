008a0ba4c635e0edb7732b286d248f81e6d8889e
Ubuntu <azureuser@evaluation-server-vm-1.mrkzp5aw111e5oyicv3kqvwt5b.ax.internal.cloudapp.net>
2021-06-03 00:57:06 +0000

    [AzureCloud] Evaluator: updated the run.sh script

--- H3/Evaluation/run.sh
+++ H3/Evaluation/run.sh
@@ -1,4 +1,8 @@
-export GOOGLE_APPLICATION_CREDENTIALS="~/gcloud-key.json"
+#!/bin/bash
+
+cp ~/gcloud-key.json ~/Uni-Cloud-Computing/H3/Evaluation/gcloud-key.json
+
+export GOOGLE_APPLICATION_CREDENTIALS="./gcloud-key.json"
 
 python3 server.py
 
